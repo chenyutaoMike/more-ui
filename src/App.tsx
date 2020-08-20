6f7a6908c6b7bd10a914daeb421cca98f05315a8
Mike <1032098274@qq.com>
2020-08-20 22:51:43 +0800

    完成Button组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button, { ButtonSize, ButtonType } from './components/Button/Button'
+import Menu from './components/Menu/Menu';
+import MenuItem from './components/Menu/MenuItem';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button className="fuck" onClick={() => { console.log('111') }}>hello</Button>
+      <Button  btnType={ButtonType.Primary} size={ButtonSize.Large}>hello</Button>
+      <Button btnType={ButtonType.Link} size={ButtonSize.Large} href="https://www.baidu.com/">link button</Button>
+
+      <Button disabled>hello</Button>
+      <hr />
+      <Menu defaultIndex={0} onSelect={(index)=>{console.log(index)}}>
+        <MenuItem index={1}>sjsjsjjsjsjsj</MenuItem>
+        <MenuItem index={2}>sjsjsjjsjsjsj</MenuItem>
+        <MenuItem index={3}>sjsjsjjsjsjsj</MenuItem>
+        <MenuItem index={4}>sjsjsjjsjsjsj</MenuItem>
+
+      </Menu>
+
     </div>
   );
 }
